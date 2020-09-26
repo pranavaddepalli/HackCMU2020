7e05a97d9b4b7e7273f6b03bd66553cd0311b340
Pranav Addepalli <pranav.addepalli@gmail.com>
2020-09-26 12:43:41 -0400

    fixing discord msg

--- server.js
+++ server.js
@@ -252,10 +252,7 @@ io.sockets.on('connection', function(socket) {
         discMsg = req.params.message
         console.log('entered post');
         console.log(discMsg);
-        io.sockets.in("room-" + socket.roomnum).emit('new message', {
-            msg: discMsg,
-            user: socket.username
-        });
+        io.sockets.in("room-" + socket.roomnum).emit('send message', discMsg);
         console.log('completed post')
         res.send("POST request receieved with message " + discMsg);
         discMsg = "";
